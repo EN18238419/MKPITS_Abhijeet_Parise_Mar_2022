a5255e4a00010eddfcc06f5aa1645ce7f6dc2cbd
EN18238419 <abhijeetparise2000@gmail.com>
2022-03-30 17:32:20 +0530

    display variable value using printf

--- programme2.C
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-int num=200;
-clrscr();
-printf("%d",num);
-getch();
-}
--- programme3.C
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-int num=520;
-clrscr();
-printf("the value is= %d",num);
-getch();
-}
