03e09f62dd4c4c748369df93e97ca3f0abfbad99
EN18238419 <abhijeetparise2000@gmail.com>
2022-04-05 22:34:23 +0530

    perimeter and area of rectangle

---
+++ Assignment1/A1P7.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int h=7,b=5,area,peri;
+clrscr();
+area=h*b;
+peri=2*(h+b);
+printf("area of rectangle= %d inches\n",area);
+printf("perimeter of rectangle= %d inches",peri);
+getch();
+}
