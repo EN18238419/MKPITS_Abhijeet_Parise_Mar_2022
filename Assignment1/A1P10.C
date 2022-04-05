c7a22ab142bcedf4ecdfd15384666732c1be442a
EN18238419 <abhijeetparise2000@gmail.com>
2022-04-05 22:36:43 +0530

    sum of two integer number

---
+++ Assignment1/A1P10.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,n2,sum;
+clrscr();
+printf("enter two integer num ber\n");
+scanf("%d%d",&n1,&n2);
+sum=n1+n2;
+printf("sum= %d",sum);
+getch();
+}
