5a8250a74842abf9d6b97d4d1110bce700c51156
EN18238419 <abhijeetparise2000@gmail.com>
2022-04-05 23:51:26 +0530

    area and perimeter of 6 inches radius

---
+++ Assignment1/A1P8.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float p=3.14,r=6,a,pe;
+clrscr();
+a=p*r*r;
+pe=2*p*r;
+printf("area of circle= %f inches\n",a);
+printf("perimeter of circle= %f inches",pe);
+getch();
+}
