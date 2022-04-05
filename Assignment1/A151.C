1de7e94055f9fcca05fca0da23d5bd3c1bfef483
EN18238419 <abhijeetparise2000@gmail.com>
2022-04-05 22:30:37 +0530

    division of two numbers

---
+++ Assignment1/A151.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float n1,n2,res;
+clrscr();
+printf("enter two number\n");
+scanf("%f%f",&n1,&n2);
+res=n1/n2;
+printf("division= %f",res);
+getch();
+}
