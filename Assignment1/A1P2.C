3a0c9a700fb71710984907570189abfb84e73d16
EN18238419 <abhijeetparise2000@gmail.com>
2022-04-05 23:37:52 +0530

    display your name,dob,mo.no

---
+++ Assignment1/A1P2.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int dob,mn;
+char name;
+clrscr();
+printf("name : abhijeet parise\n");
+printf("dob  :august 21,2000\n");
+printf("mo.no:7821981676");
+getch();
+}
