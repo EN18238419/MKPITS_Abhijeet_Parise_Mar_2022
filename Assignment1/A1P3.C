61a18160e2f3e6b0e9eb784ffbe3b9f98a42955a
EN18238419 <abhijeetparise2000@gmail.com>
2022-04-05 22:25:40 +0530

    sum of two numbers

---
+++ Assignment1/A1P3.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float n1,n2,sum;
+clrscr();
+printf("enter two number\n");
+scanf("%f%f",&n1,&n2);
+sum=n1+n2;
+printf("sum= %f",sum);
+getch();
+}
