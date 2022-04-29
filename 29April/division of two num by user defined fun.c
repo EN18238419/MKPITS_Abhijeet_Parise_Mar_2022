e0ef77174e746d9ea968cfa0b4ba5e9339478e6b
EN18238419 <abhijeetparise2000@gmail.com>
2022-04-29 22:06:51 +0530

    division of two numbers by user defined function

---
+++ 29April/division of two num by user defined fun.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void div();
+int main()
+{
+	div();
+}
+void div()
+{
+float p,s,res;
+printf("enter two number\n");
+scanf("%f%f",&p,&s);
+res=p/s;
+printf("division= %f",res);
+}
