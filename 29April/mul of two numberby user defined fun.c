51c7daf640c79f02f2600176a1ae9faa55bbdfa1
EN18238419 <abhijeetparise2000@gmail.com>
2022-04-29 22:05:25 +0530

    multiplication of two numbers by user defined function

---
+++ 29April/mul of two numberby user defined fun.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	mul();
+}
+void mul()
+{
+int p,s,res;
+printf("enter two number\n");
+scanf("%d%d",&p,&s);
+res=p*s;
+printf("multiplication= %d",res);
+}
