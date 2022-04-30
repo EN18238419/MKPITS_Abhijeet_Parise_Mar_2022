9efd7deb8eb5aafce4cf30fa6bc69798b076196b
EN18238419 <abhijeetparise2000@gmail.com>
2022-04-30 22:48:27 +0530

    menuedriven program (by user defined function)

---
+++ 30April/4.menuedriven program by user defined fun.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+void fun();
+int main()
+{
+	fun();
+}
+void fun()
+{
+float n1,n2,res;int option;
+printf("press 1 for addition\n");
+printf("press 2 for substraction\n");
+printf("press 3 for multiplication\n");
+printf("press 4 for division\n");
+printf("enter your choice\n");
+scanf("%d",&option);
+switch(option)
+{
+case 1:
+printf("enter two number\n");
+scanf("%f%f",&n1,&n2);
+res=n1+n2;
+printf("addition= %f",res);
+break;
+case 2:
+printf("enter two number\n");
+scanf("%f%f",&n1,&n2);
+res=n1-n2;
+printf("substraction= %f",res);
+break;
+case 3:
+printf("enter two number\n");
+scanf("%f%f",&n1,&n2);
+res=n1*n2;
+printf("multiplication= %f",res);
+break;
+case 4:
+printf("enter two number\n");
+scanf("%f%f",&n1,&n2);
+res=n1/n2;
+printf("division= %f",res);
+break;
+default:
+printf("invalid choice");
+}
+getch();
+}
+
