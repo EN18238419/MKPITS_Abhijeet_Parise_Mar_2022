93de73e6fa836006065d6ea0f4787ceb47969593
EN18238419 <abhijeetparise2000@gmail.com>
2022-04-30 22:57:02 +0530

    add two matrix (by user defined function)

---
+++ 30April/9.add two matrix by user def fun.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdio.h>
+void fun();
+int main()
+{
+	fun();
+}
+void fun()
+{
+	int m1[2][2], m2[2][2], res[2][2],i,j;
+	printf("enter 2 X 2 matrix m1:\n");
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			scanf("%d",&m1[i][j]);
+		}
+	}
+		printf("enter 2 X 2 matrix m2:\n");
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			scanf("%d",&m2[i][j]);
+		}
+	}
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			res[i][j]=m1[i][j]+m2[i][j];
+		}
+	}
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+		printf("m1[%d][%d]=%d\n",i,j,res[i][j]);
+		}
+	}
+	
+	
+}
+
