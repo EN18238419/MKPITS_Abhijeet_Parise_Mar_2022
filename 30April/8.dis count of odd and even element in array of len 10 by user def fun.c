9740aae0a8af46fa50202315ef9ab1c8c5cddc6f
EN18238419 <abhijeetparise2000@gmail.com>
2022-04-30 22:55:53 +0530

    display count of odd and even element array of length 10 (by user defined function)

---
+++ 30April/8.dis count of odd and even element in array of len 10 by user def fun.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+void fun();
+int main()
+{
+	fun();
+}
+void fun()
+{
+	int a[10],i,ec=0,oc=0;
+	printf("enter 10 number\n");
+	for(i=0;i<=9;i++)
+	{
+		printf("a[%d]:",i);
+		scanf("%d",&a[i]);
+		
+	}
+	printf("\n");
+	for(i=0;i<=9;i++)
+	{
+	if(a[i]%2==0)	
+	{
+		ec=ec+1;
+	}
+	else
+	{
+		oc=oc+1;
+	}
+	}
+	printf("even: %d\n",ec);
+	printf("odd: %d",oc);
+	
+	
+}
+
+
