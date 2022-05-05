8b9a8ed3754f48d8cfa13214ce7bdbf6fa33911f
EN18238419 <abhijeetparise2000@gmail.com>
2022-05-05 09:10:18 +0530

    menue driven program of add,sub,mul,div by user defined fun with returntype and no argument

---
+++ 5may/4.4 menue driven program .c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<conio.h>
+float fun()
+{
+int option;
+float res,n1,n2;
+printf("press 1 for addition\n");
+printf("press 2 for substraction\n");
+printf("press 3 for multiplication\n");
+printf("press 4 for division\n");
+printf("enter your choice\n");
+scanf("%d",&option);
+switch(option)
+{
+case 1:
+printf("enter two number\n");
+scanf("%f%f",&n1,&n2);
+res=n1+n2;
+return res;
+break;
+case 2:
+printf("enter two number\n");
+scanf("%f%f",&n1,&n2);
+res=n1-n2;
+return res;
+break;
+case 3:
+printf("enter two number\n");
+scanf("%f%f",&n1,&n2);
+res=n1*n2;
+return res;
+break;
+case 4:
+printf("enter two number\n");
+scanf("%f%f",&n1,&n2);
+res=n1/n2;
+return res;
+break;
+default:
+return 0;
+}
+}
+int main()
+{
+
+float g;
+g=fun();
+if(g!=0)
+{
+	printf("result: %f",g);
+}
+else
+{
+	printf("invalid choice");
+}
+}
+
+
