e396f8a6e9f031ae5ad968eb06667de7d66939ce
EN18238419 <abhijeetparise2000@gmail.com>
2022-05-05 23:50:21 +0530

    compute the sum of two integer ,if sum is in the range of 10-20 inclusive return 30

---
+++ Assignment10/91. compute sum of two integer , if sum is in range 10-20 inclusive return 30.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int x1,x2,sum;
+	printf("enter two number\n");
+	scanf("%d%d",&x1,&x2);
+	sum=x1+x2;
+	if(sum>=10&&sum<=20)
+	{
+		printf("30");
+		
+	}
+	else
+    {
+	    printf("%d",sum);
+    }
+}
