e98db66f56aafb2d23de99763fccc240dbb6b62d
EN18238419 <abhijeetparise2000@gmail.com>
2022-05-08 19:35:43 +0530

    check whether sequence of a number 1,2,3 appear in an array or not

---
+++ Assignment9/check whether sequence of a numner 1,2,3 appear in array or not .c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,a[9],f=0;
+	printf("enter the element of an array\n");
+	for(i=0;i<9;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<9;i++)
+	{
+	 if(a[i]==1&&a[i+1]==2&&a[i+2]==3)
+	 {
+	 	printf("sequence appear");
+	 	f=1;
+	 }
+	}
+	if(f==0)
+	printf("sequence not appear");
+}
