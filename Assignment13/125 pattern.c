86845e6042339b456e9ac59b0c540c79d7103c8b
EN18238419 <abhijeetparise2000@gmail.com>
2022-05-10 20:43:48 +0530

    pattern 296

---
+++ Assignment13/125 pattern.c
@@ -0,0 +1,22 @@
+//EDCBA
+//EDCB
+//EDC
+//ED
+//E
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r,c,add=6;
+	for(r=5;r>=1;r--)
+	{
+		for(c=1;c<=r;c++)
+		{
+		add=add-1;	
+		printf("%c",(add+64));
+		}
+		add=6;
+		printf("\n");
+	}
+	
+}
