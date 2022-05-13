16963f0ab0c33888847a9cf8b7c73ba527eec4b5
EN18238419 <abhijeetparise2000@gmail.com>
2022-05-13 19:48:15 +0530

    324 pattern

---
+++ Assignment15/144 pattern.c
@@ -0,0 +1,26 @@
+//   A
+//  B B
+// C C C
+//D D D D
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r,c;
+	for(r=1;r<=4;r++)
+	{
+		for(c=1;c<=4;c++)
+		{
+			if(r+c>4)
+			{
+				printf("%c ",(r+64));
+				
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
