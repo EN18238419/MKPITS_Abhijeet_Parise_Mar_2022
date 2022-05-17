e4826ede276c0c6474d0e788fe4f7f75aa76fd2b
EN18238419 <abhijeetparise2000@gmail.com>
2022-05-17 09:40:32 +0530

    return true if given array contain three increasing adjecent numbers

---
+++ Assignment16/133 return true if given array contain three increasing adjecent numbers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,a[10],size,f=0;
+	printf("enter the size of array: ");
+	scanf("%d",&size);
+	printf("enter array element\n");
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<size;i++)
+	{
+		if(a[i+1]>a[i]&&a[i+2]>a[i+1])
+		{
+			f=1;
+		}
+	}
+	if(f==1)
+	{
+		printf("true");
+	}
+	else
+	{
+		printf("false");
+	}
+}
