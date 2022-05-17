4d782448eb18ec79a92eac8197ca9ea23f8e3318
EN18238419 <abhijeetparise2000@gmail.com>
2022-05-17 22:08:30 +0530

    pattern no.418

---
+++ Assignment17/pattern 418.c
@@ -0,0 +1,39 @@
+//G G G G G G G 
+//  E E E E E
+//    C C C
+//      A
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int r,c,b,add=71,add1=71;
+	for(r=4;r>=1;r--)
+	{
+		for(c=1;c<=4;c++)
+		{
+			if(r+c>4)
+			{
+				printf("%c ",add);
+			}
+			else
+			{
+			printf("  ");
+		    }
+	   }
+	   add=add-2;
+		for(b=3;b>=1;b--)
+		{
+			if(r+b>4)
+			{
+				printf("%c ",add1);
+				
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		add1=add1-2;
+	    printf("\n");
+	}
+}
