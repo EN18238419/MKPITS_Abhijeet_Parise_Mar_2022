8fcbd8d80c98b05f9273e1a7b92cf0ae702d787f
EN18238419 <abhijeetparise2000@gmail.com>
2022-05-19 10:26:27 +0530

    return true if specified number of same element appear at the start and end of given array

---
+++ Assignment16/132 return true if specified number of same element appears at the start and end of given array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,n,f=0,temp,a[10];
+	printf("enter array size: ");
+	scanf("%d",&n);
+	printf("enter array element\n");\
+	for(i=0;i<n;i++)
+	{
+		printf("a[%d]= ",i);
+		scanf("%d",&a[i]);
+	}
+	i=0;
+	temp=a[0];
+	while(a[i]==temp&&a[i]==a[(n-1)-i])
+	{
+		f=f+1;
+		i++;
+	}
+	if(f>0)
+	{
+		printf("true");
+	}
+	else
+	{
+		printf("false");
+	}
+}
