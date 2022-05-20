9f5816d5990434f3641c476160ac4db933c84070
EN18238419 <abhijeetparise2000@gmail.com>
2022-05-20 14:36:19 +0530

    display number in rectangle of 3 column wide and 5 rows tall

---
+++ Assignment18/146 dis number in rectangle of 3 column wide and 5 rows tall.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int n ;
+   printf("enter any number: ");
+   scanf("%d",&n);
+   printf("%d %d %d\n",n,n,n);
+   printf("%d   %d\n",n,n);
+   printf("%d   %d\n",n,n);
+   printf("%d   %d\n",n,n);
+   printf("%d %d %d\n",n,n,n);
+   	
+}
