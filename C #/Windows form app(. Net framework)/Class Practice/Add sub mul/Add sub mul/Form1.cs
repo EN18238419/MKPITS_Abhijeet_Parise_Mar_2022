97dd48cababd42870e9aba7f901b32fb3b221440
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-03 08:34:18 +0530

    Addition ,Substraction and multiplication of two numbers

---
+++ C #/Windows form app(. Net framework)/Class Practice/Add sub mul/Add sub mul/Form1.cs
@@ -0,0 +1,34 @@
+namespace Add_sub_mul
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1=Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int res = n1 + n2;
+            label3.Text= "Addition= " +res;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int res = n1 - n2;
+            label3.Text = "Substraction= " + res;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int res = n1 * n2;
+            label3.Text = "Multiplication= " + res;
+        }
+    }
+}
