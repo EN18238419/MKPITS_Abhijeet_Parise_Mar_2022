0e1c454bca78bb4ec82b2cccf9611f427aa9d181
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 09:21:01 +0530

    dispaly withdrawl,deposit and show money

---
+++ Windows Form/dispaly withdrawl,deposit and show money/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bank b = new bank();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            b.act_no = Convert.ToInt64(textBox1.Text);
+            string res = b.deposit(Convert.ToInt32(textBox2.Text));
+            label3.Text = res;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            b.act_no = Convert.ToInt64(textBox1.Text);
+            string res = b.withdrawl(Convert.ToInt32(textBox2.Text));
+            label3.Text = res;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            b.act_no = Convert.ToInt64(textBox1.Text);
+            string res = b.showbalance();
+            label3.Text = "Account no: "+b.act_no+ ": "+res;
+        }
+    }
+}
---
+++ Windows Form/dispaly withdrawl,deposit and show money/WindowsFormsApp2/bank.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    internal class bank
+    {
+        public long act_no;
+        public int amount;
+        public int bal=1000;
+        public string deposit(int amount)
+        {
+            bal=bal+amount;
+            return "Amount deposited successfully, balance is " + bal;
+        }
+        public string withdrawl( int amount)
+        {
+            if(amount>bal)
+            {
+                return "Insufficient fund! , money cannot withdrwal";
+            }
+            else
+            {
+                bal = bal - amount;
+                return "Amount deposited successfully, balance is " + bal;
+            }
+           
+        }
+        public string showbalance()
+        {
+           
+            return " Balance is " + bal;
+        }
+    }
+}
