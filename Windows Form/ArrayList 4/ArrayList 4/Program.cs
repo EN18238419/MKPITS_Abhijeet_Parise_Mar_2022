227591786455ac439eb01f3d23379c6bf0486fce
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:09:00 +0530

    ArrayList 4

---
+++ Windows Form/ArrayList 4/ArrayList 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+
+namespace ArrayList_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr=new ArrayList();
+            arr.Add(11);
+            arr.Add(12);
+            arr.Add(13);
+           foreach(int i in arr)
+            {
+                Console.WriteLine("number: "+i);
+            }
+        }
+    }
+}
