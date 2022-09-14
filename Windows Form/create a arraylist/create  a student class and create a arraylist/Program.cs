da06d0799db3172434e2af5056bd885b2ec3b3d8
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:13:36 +0530

    create a arraylist

---
+++ Windows Form/create a arraylist/create  a student class and create a arraylist/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+
+namespace create__a_student_class_and_create_a_arraylist
+{
+    public class Student
+        {
+        public int roll_no;
+        public string name;
+
+        public Student(int roll_no, string name)
+        {
+            this.roll_no = roll_no;
+            this.name = name;
+        }
+    }
+ internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(02, "Abhi");
+            Student s2 = new Student(03, "Abhijeet");
+            Student s3 = new Student(04, "Krishna");
+            ArrayList arr=new ArrayList();
+            arr.Add(s1);    
+            arr.Add(s2);
+            arr.Add(s3);
+
+            foreach(Student s in arr)
+            {
+                Console.WriteLine("Roll number: "+s.roll_no );
+                Console.WriteLine("Name: " + s.name);
+            }
+        }
+    }
+}
