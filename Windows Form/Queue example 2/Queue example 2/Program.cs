8fa85bb3c3018a548faa4bdcb91f75715792d811
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:21:33 +0530

    Queue example 2

---
+++ Windows Form/Queue example 2/Queue example 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+namespace Queue_example_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+
+            q.Enqueue("AAA");
+            q.Enqueue("BBB");
+            q.Enqueue("CCC");
+            q.Enqueue("DDD");
+
+            Console.Write("Current queue:\t\t");
+            foreach (string c in q)
+            {
+                Console.Write(c + "\t");
+            }
+
+            Console.WriteLine("\n");
+            q.Enqueue("EEE");
+            q.Enqueue("FFF");
+            Console.Write("queue after enqueue:\t");
+            foreach (string c in q)
+            {
+                Console.Write(c + "\t");
+            }
+            Console.WriteLine("\n");
+
+
+            Console.WriteLine("Removing some values: ");
+            string ch = (string)q.Dequeue();
+            Console.WriteLine("The removed value: {0}", ch);
+
+            ch = (string)q.Dequeue();
+            Console.WriteLine("The removed value: {0}", ch);
+
+
+
+        }
+    }
+}
