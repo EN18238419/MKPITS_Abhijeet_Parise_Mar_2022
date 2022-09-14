156c8d3d69e76439f268f6567dde60e9b55dde86
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 09:22:40 +0530

    display employee info

---
+++ Windows Form/display employee info/WindowsFormsApp5/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp5
+{
+    internal class Employee
+    {
+        public int no;
+        public string name;
+        public int b_sal;
+
+        public float Calculate_bonus(int b_sal)
+        {
+            float bonus=b_sal*0.45f;
+            return bonus;
+        }
+        public float Calculate_total_sal(float bonus,int b_sal)
+        {
+            float t_sal = bonus + b_sal;
+            return t_sal;
+        }
+    }
+}
---
+++ Windows Form/display employee info/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Employee emp = new Employee();
+            float bonus=emp.Calculate_bonus(Convert.ToInt32(textBox3.Text));
+            float t_sal=emp.Calculate_total_sal(Convert.ToSingle(bonus), Convert.ToInt32(textBox3.Text));
+            emp.no=Convert.ToInt32(textBox1.Text);
+            emp.name = textBox2.Text;
+            label7.Text = emp.no.ToString();
+            label8.Text = emp.name;
+            label9.Text = t_sal.ToString();
+        }
+    }
+}
