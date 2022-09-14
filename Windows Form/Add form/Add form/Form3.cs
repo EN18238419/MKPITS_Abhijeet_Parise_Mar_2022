083c78866eed8009fdac5fee60c1e28592e2d8a4
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 07:54:07 +0530

    Add form

---
+++ Windows Form/Add form/Add form/Form3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Add_form
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            int sq = n * n;
+            label2.Text = "square " + sq;
+        }
+    }
+}
