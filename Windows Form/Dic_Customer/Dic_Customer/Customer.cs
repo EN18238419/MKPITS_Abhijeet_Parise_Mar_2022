e54638b4b9cde6ceaa73abab6dbf5cae270c8387
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:27:23 +0530

    Dictionary_customer

---
+++ Windows Form/Dic_Customer/Dic_Customer/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dic_Customer
+{
+     class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Customer(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
