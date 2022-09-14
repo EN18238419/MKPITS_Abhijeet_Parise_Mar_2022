e3c2efdefd5df178096bfc99c78642be30e0c07a
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:55:32 +0530

    display greatest among three

---
+++ Windows Form/ASSIGNMENTS/display greatest among three/display greatest among three/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace display_greatest_among_three
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1=Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            int n3 = Convert.ToInt32(textBox3.Text);
+            if(n1>n2 && n1>n3)
+            {
+                label5.Text = "Greatest number " + n1;
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                label5.Text = "Greatest number " + n2;
+            }
+            else
+            {
+                label5.Text = "Greatest number " + n3;
+            }
+        }
+    }
+}
