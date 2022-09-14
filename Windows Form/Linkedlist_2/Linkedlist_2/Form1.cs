af154a6577181d3eced65ee41646e61349a8423b
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:31:35 +0530

    Linkedlist_2

---
+++ Windows Form/Linkedlist_2/Linkedlist_2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Collections;
+namespace Linkedlist_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        LinkedList<string> list = new LinkedList<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            list.Remove(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            list.AddLast(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            list.AddFirst(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            StringBuilder str = new StringBuilder();
+            foreach(string s in list)
+            {
+                str.Append("name: "+s+"\n");
+            }
+            label2.Text=str.ToString();
+        }
+    }
+}
