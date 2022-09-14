8fdb66f6217bcbfd03003f071ba2fba02789e774
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:09:46 +0530

    ArrayList 5

---
+++ Windows Form/ArrayList 5/ArrayList 5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using System.Collections;   // this must be used for arraylist class
+
+namespace ArrayList_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList();
+            arr.Add(21);
+            arr.Add(17);
+            arr.Add(32);
+            Console.WriteLine("without sorting");
+            foreach (int i in arr)
+            {
+                Console.WriteLine("number:" + i);
+            }
+            Console.WriteLine();
+            Console.WriteLine("with sorting");
+            arr.Sort();
+            foreach (int i in arr)
+            {
+                Console.WriteLine("number:" + i);
+            }
+
+        }
+    }
+}
