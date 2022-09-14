ae11af5dd938e91a6ca5b87527fd3bcbc3a99405
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:45:51 +0530

    pattern ex1

---
+++ Windows Form/ASSIGNMENTS/pattern ex1/pattern ex1/Form1.cs
@@ -0,0 +1,43 @@
+﻿// 1 1 1
+// 2 2 2
+// 3 3 3
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pattern_ex1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder str=new StringBuilder();
+            for(int i=1; i<=3;i++)
+            {
+                for(int j=1; j<=3;j++)
+                {
+                    str.Append(i + " ");
+                }
+                str.Append("\n");
+            }
+            label1.Text=str.ToString();
+        }
+    }
+}
