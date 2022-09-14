bf8422a32a6f0ed24d4ceb8f2c7e328b58b34b06
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 09:23:31 +0530

    display order details

---
+++ Windows Form/display order details/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Order order = new Order();
+            order.order_no=Convert.ToInt32(textBox1.Text);
+            order.customer_name = textBox2.Text;
+            order.product_name = textBox3.Text;
+            
+            label10.Text = order.order_no.ToString();
+            label11.Text = order.customer_name;
+            label12.Text=order.product_name;
+            float total_amount= order.total_amount(Convert.ToSingle(textBox4.Text),Convert.ToInt32(textBox5.Text));
+            label13.Text= total_amount.ToString();  
+        }
+    }
+}
---
+++ Windows Form/display order details/WindowsFormsApp6/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp6
+{
+    internal class Order
+    {
+        public int order_no;
+        public string customer_name;
+        public string product_name;
+        public float price;
+        public int quantity;
+
+        public float total_amount(float price,int quantity)
+        {
+            this.price = price;
+            this.quantity = quantity;
+            return price*quantity;
+        }
+    }
+}
