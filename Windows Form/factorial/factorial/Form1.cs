7e79f6ca8d13d2139040f91a4c718f4f974b7f64
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 07:36:04 +0530

    factorial

---
+++ Windows Form/factorial/factorial/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace factorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num=Convert.ToInt32(textBox1.Text);
+            int fact = 1;
+            for(int i=num;i>0;i--)
+            {
+                fact = fact * i;
+            }
+            label2.Text = "Factorial= " + fact;
+        }
+    }
+}
