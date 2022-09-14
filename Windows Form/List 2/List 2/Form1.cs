04ebcea623fdb4237c17a23cca64efff293a4002
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:33:06 +0530

    List 2

---
+++ Windows Form/List 2/List 2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> list = new List<string>();
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            list.Add(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            list.Remove(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            list.Insert(3,textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(list.Contains(textBox1.Text))
+            {
+                label2.Text = "Element Found";
+            }
+            else
+            {
+                label2.Text = "Element not Found";
+            }
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            list.Sort();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            StringBuilder str = new StringBuilder();
+            foreach(string s in list)
+            {
+                str.Append("Name: "+s+"\n");
+            }
+            label2.Text=str.ToString();
+        }
+    }
+}
