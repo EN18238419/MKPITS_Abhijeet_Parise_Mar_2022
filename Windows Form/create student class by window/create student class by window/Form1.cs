7cfbed2a674fb0158db273032bc0836469c5d4bc
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:14:23 +0530

    create student class by window

---
+++ Windows Form/create student class by window/create student class by window/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Collections;  //
+
+namespace create_student_class_by_window
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList arr=new ArrayList();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student s1 = new Student(Convert.ToInt32(textBox1.Text), textBox2.Text);
+            Student s2 = new Student(Convert.ToInt32(textBox3.Text), textBox4.Text);
+            arr.Add(s1);
+            arr.Add(s2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder str = new StringBuilder();
+            foreach (Student s in arr)
+            {
+                str.Append("rno " + s.roll_no + " name " + s.name + "\n");
+            }
+            label5.Text = str.ToString();
+
+        }
+    }
+}
---
+++ Windows Form/create student class by window/create student class by window/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_student_class_by_window
+{
+    internal class Student
+    {
+        public int roll_no;
+        public string name;
+       
+
+        public Student(int roll_no, string name)
+        {
+            this.roll_no = roll_no;
+            this.name = name;
+        }
+    }
+}
