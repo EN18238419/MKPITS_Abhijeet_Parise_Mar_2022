79181b7f10f76fb3c11fba916ac0df9d0705f713
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 09:25:13 +0530

    display total,percentage and grade

---
+++ Windows Form/display total,percentage and grade/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Result res=new Result();
+           // res.phy=Convert.ToInt32(textBox1.Text);
+           // res.che = Convert.ToInt32(textBox2.Text);
+           // res.math = Convert.ToInt32(textBox3.Text);
+            int tot = res.calculate_total(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+            label7.Text =tot.ToString();
+            float perc = res.calculate_per(Convert.ToInt32(tot));
+            label8.Text = perc.ToString();
+            string gr = res.calculate_grade(Convert.ToSingle(perc));
+            label9.Text = gr;
+        }
+    }
+}
---
+++ Windows Form/display total,percentage and grade/WindowsFormsApp4/Result.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    internal class Result
+    {
+        public int phy;
+        public int che;
+        public int math;
+
+        public int calculate_total(int phy,int che,int math)
+        {
+            int total=phy+che+math;
+            return total;
+        }
+         public float calculate_per(int total)
+          {
+
+              float per = (total / 300.0f) * 100;
+              return  per;
+          }
+        public string calculate_grade(float per)
+        {
+            string grade = null;
+            
+            if(per>75)
+            {
+                grade = "Distinction";
+            }
+            else if(per>=60&&per<75)
+            {
+                grade = "First Division";
+            }
+            else if (per >= 40 && per < 60)
+            {
+                grade = "Second Division";
+            }
+            else
+            {
+                grade = "Fail";
+            }
+            return grade;
+        }
+    }
+}
