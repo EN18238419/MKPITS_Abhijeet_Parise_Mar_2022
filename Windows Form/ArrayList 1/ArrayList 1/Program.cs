61105c2d21b6d89ba48bede02585e1ccd740e48b
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:06:18 +0530

    ArrayList 1

---
+++ Windows Form/ArrayList 1/ArrayList 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+
+namespace ArrayList_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList();
+            arr.Add("Krishna");
+            arr.Add("Abhijeet");
+            arr.Add("Pardeep");
+
+            foreach(Object obj in arr)
+            {
+                Console.WriteLine("Student name: "+obj);
+            }
+        }
+    }
+}
