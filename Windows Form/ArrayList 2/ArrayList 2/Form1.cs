e0732dc812e3647e25ad46973ae381d67e73cc30
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:07:06 +0530

    ArrayList 2

---
+++ Windows Form/ArrayList 2/ArrayList 2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Collections;
+
+namespace ArrayList_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList arr=new ArrayList();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            arr.Add(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder str=new StringBuilder();
+            foreach(object o in arr)
+            {
+                str.Append("Name: " + o + " \n");
+            }
+            label2.Text=str.ToString();
+        }
+    }
+}
