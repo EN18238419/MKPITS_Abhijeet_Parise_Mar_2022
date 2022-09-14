171f0ffc90b3631e24a5c8e1daa465450a7ca2f4
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:20:42 +0530

    Queue

---
+++ Windows Form/Queue/Queue/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+namespace Queue_example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+
+            q.Enqueue('A');
+            q.Enqueue('B');
+            q.Enqueue('C');
+            q.Enqueue('D');
+
+            Console.Write("Current Queue:\t\t");
+            foreach (char c in q)
+            {
+                Console.Write(c + "\t");
+            }
+            Console.WriteLine();
+            // Adding element
+            q.Enqueue('E');
+            q.Enqueue('F');
+
+            Console.WriteLine();
+            Console.Write("Queue After Enqueue:\t");
+
+            foreach (char c in q)
+            {
+                Console.Write(c + "\t");
+            }
+
+            Console.WriteLine();
+
+            // Removing element
+            q.Dequeue();
+            q.Dequeue();
+            q.Dequeue();
+            Console.WriteLine();
+            Console.Write("Queue After Dequeue:\t");
+            foreach (char c in q)
+            {
+                Console.Write(c + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
