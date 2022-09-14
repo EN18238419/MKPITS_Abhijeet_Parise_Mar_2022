43dad1384b474787a648d815810b3db8d66a2b54
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:10:25 +0530

    ArrayList 6

---
+++ Windows Form/ArrayList 6/ArrayList 6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+
+namespace ArrayList_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr = new ArrayList();
+            arr.Add(21);
+            arr.Add(17);
+            arr.Add(32);
+            Console.WriteLine("given arraylist");
+            foreach (int i in arr)
+            {
+                Console.WriteLine("number:" + i);
+            }
+            Console.WriteLine();
+    
+           /* Console.WriteLine("array position : ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("value to insert: ");
+            int val = Convert.ToInt32(Console.ReadLine());
+            arr.Insert(index, val);*/
+            Console.WriteLine("arraylist after insertion");
+            arr.Insert(1,75);
+
+            foreach (int i in arr)
+            {
+                Console.WriteLine("number:" + i);
+            }
+        }
+    }
+}
