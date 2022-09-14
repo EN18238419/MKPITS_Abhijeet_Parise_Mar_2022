8b10d3b122e1eeb3fc684a4a2bad9a35e6cce17d
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:22:23 +0530

    sorted list ex 1

---
+++ Windows Form/sorted list ex 1/sorted list ex 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+namespace sorted_list_ex_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList sl=new SortedList();
+
+            sl.Add("6", "fg");
+            sl.Add("7", "gh");
+            sl.Add("4", "de");
+            sl.Add("5", "ef");
+            sl.Add("1", "ab");
+            sl.Add("2", "bc");
+            sl.Add("3", "cd");
+
+            if (sl.ContainsValue("hi"))
+            {
+                Console.WriteLine("This student name is already in the list");
+            }
+            else
+            {
+                sl.Add("8", "hi");
+            }
+
+             
+            ICollection key = sl.Keys;
+
+            foreach (string k in key)
+            {
+                Console.WriteLine(k + "\t" + sl[k]);
+            }
+
+        }
+    }
+}
