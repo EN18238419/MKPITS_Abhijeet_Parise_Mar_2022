61a5c99cb2975994a847e48595637f6a9466ccd7
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:42:18 +0530

    Calculate

---
+++ Windows Form/ASSIGNMENTS/calculate/calculate/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculate
+{
+    public class Calculate
+    {
+        int n1;
+        int n2;
+
+        public int addition(int n1,int n2)
+        {
+            int sum = n1 + n2;
+            return sum;
+        }
+        public int substraction(int n1, int n2)
+        {
+            int sub = n1 - n2;
+            return sub;
+        }
+    }
+}
