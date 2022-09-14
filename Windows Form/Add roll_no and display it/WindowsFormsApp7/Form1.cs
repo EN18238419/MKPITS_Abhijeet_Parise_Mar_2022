4fa914cd6698fad9a40c603c55e5ab82bd0270a7
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 09:26:01 +0530

    Add roll_no and display it

---
+++ Windows Form/Add roll_no and display it/WindowsFormsApp7/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Collections;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList arr = new ArrayList();
+        student[] stud = new student[3];
+        int i = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            i++;
+            if (i <= 3)
+            {
+                stud[i - 1] = new student(Convert.ToInt32(textBox1.Text), textBox2.Text);
+                arr.Add(stud[i - 1]);
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox1.Focus();
+
+            }
+            if (i == 3)
+            {
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                button1.Enabled = false;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (student s in arr)
+            {
+                sb.Append("Roll no: " + s.roll_no + "   Name: " + s.name + "\n");
+            }
+            label3.Text = sb.ToString();
+
+        }
+    }
+}
---
+++ Windows Form/Add roll_no and display it/WindowsFormsApp7/student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp7
+{
+    public class student
+    {
+        public int roll_no;
+        public string name;
+
+        public student(int roll_no, string name)
+        {
+            this.roll_no = roll_no;
+            this.name = name;
+        }   
+    }
+}
