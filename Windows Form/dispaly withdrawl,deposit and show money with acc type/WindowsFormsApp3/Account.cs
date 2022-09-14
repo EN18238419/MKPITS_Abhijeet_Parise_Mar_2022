6dd99d60ddcadd347d68a0e2395ed9ca26dbca2f
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 09:21:49 +0530

    dispaly withdrawl,deposit and show money with acc type

---
+++ Windows Form/dispaly withdrawl,deposit and show money with acc type/WindowsFormsApp3/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    abstract class Account
+    {
+        public long act_no;
+        public int bal = 2000;
+        public int amount = 0;
+        public abstract string deposit(int amount);
+
+        public string withdrawl(int amount)
+        {
+            if (amount > bal)
+            {
+                return "Insufficient Fund!, Balance is: " + bal;
+            }
+            else
+            {
+                bal = bal - amount;
+                return "Amount withdrawl successfully, Balance is: " + bal;
+            }
+
+
+        }
+        public string showbalance()
+        {
+            return "Balance is: " + bal;
+        }
+    }
+        class saving:Account
+        {
+            public override string deposit(int amount)
+            {
+                int interest = 300;
+                bal = bal + amount + interest;
+                return "Amount deposited successfully, balance is: " + bal;
+            }
+        }
+    class current : Account
+    {
+        public override string deposit(int amount)
+        {
+            bal = bal + amount;
+            return "Amount deposited successfully, balance is: " + bal;
+        }
+    }   
+    
+}
---
+++ Windows Form/dispaly withdrawl,deposit and show money with acc type/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Account account = null;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            account.act_no = Convert.ToInt64(textBox1.Text);
+            string res = account.withdrawl(Convert.ToInt32(textBox2.Text));
+            label4.Text = res;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            account.act_no = Convert.ToInt64(textBox1.Text);
+            string res = account.deposit(Convert.ToInt32(textBox2.Text));
+            label4.Text= res;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            account.act_no = Convert.ToInt64(textBox1.Text);
+            string res = account.showbalance();
+            label4.Text = "Account no: "+account.act_no+"  : "+res;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "saving")
+            {
+                account = new saving();
+            }
+            else if (comboBox1.Text == "current")
+            {
+                account = new current();
+            }
+
+        }
+    }
+}
