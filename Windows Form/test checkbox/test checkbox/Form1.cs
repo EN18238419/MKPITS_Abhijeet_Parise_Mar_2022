80e05baea5ef75d9e92ac3d1738b351ffbb717d2
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:02:25 +0530

    test checkbox

---
+++ Windows Form/test checkbox/test checkbox/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_checkbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string emp_name = textBox1.Text;
+            float b_sal=Convert.ToSingle(textBox2.Text);
+            float bonus = b_sal * 0.45f;
+            float t_sal = b_sal + bonus;
+            label3.Text = "";
+            label4.Text = "";
+            if (checkBox1.Checked)
+            {
+                label3.Text = "Bonus "+bonus;
+            }
+            if (checkBox2.Checked)
+            {
+                label4.Text = "Total salary " + t_sal;
+            }
+        }
+    }
+}
