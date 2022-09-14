6c47b75b1b88516f70376e7b47f979053738498c
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:32:14 +0530

    Linkedlist_1

---
+++ Windows Form/Linkedlist_1/Linkedlist_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkedlist_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(35);
+            list.AddLast(33);
+            list.AddLast(31);
+            list.AddLast(32);
+            list.AddFirst(38);
+            list.AddFirst(48);
+            list.AddFirst(28);
+
+            LinkedListNode<int> node = list.Find(48);
+            list.AddBefore(node, 99);
+
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
