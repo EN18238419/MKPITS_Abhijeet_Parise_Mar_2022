457fb2ce8660b2205218f9ceb964490b064541ae
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 07:50:42 +0530

    pattern practice 1

---
+++ Windows Form/pattern practice 1/pattern practice 1/Form1.cs
@@ -0,0 +1,40 @@
+﻿/*
+            *
+            * *
+            * * *
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pattern_practice_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder str = new StringBuilder();
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    str.Append("*" + " ");
+                }
+                str.Append("\n");
+            }
+            label1.Text = str.ToString();
+        }
+    }
+}
