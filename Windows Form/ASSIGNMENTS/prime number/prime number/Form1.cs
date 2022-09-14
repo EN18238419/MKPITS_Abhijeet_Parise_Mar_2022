e7ebb44296c1a3bffd7f73b1df5c75b64d700788
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-14 08:47:16 +0530

    prime number

---
+++ Windows Form/ASSIGNMENTS/prime number/prime number/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prime_number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            int num=Convert.ToInt32(textBox1.Text);
+            for(int i=1;i<=num;i++)
+            {
+                if(num%i==0)
+                {
+                    count++;
+                }
+            }
+            if(count==2)
+            {
+                label2.Text = "Prime number";
+            }
+            else
+            {
+                label2.Text = "Not a prime number";
+            }
+        }
+    }
+}
