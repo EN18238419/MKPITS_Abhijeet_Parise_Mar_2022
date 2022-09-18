4bece5fe0e0ae073c45683cf62bc62f12c6d8184
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-18 22:54:39 +0530

    create table product

---
+++ SQL Server/create table product.sql
@@ -0,0 +1,4 @@
+create table product(p_id int,p_name varchar(10),p_price int, p_quantity int)
+insert into product values(02,'Abhi',200,3)
+insert into product values(04,'Abhijeet',220,5)
+select * from product
