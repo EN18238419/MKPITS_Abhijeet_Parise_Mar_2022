6fcdd21e2a65458286741e03b98ad26d11a9a50b
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-18 22:55:19 +0530

    create table student

---
+++ SQL Server/create table student.sql
@@ -0,0 +1,6 @@
+                    --sample_db database
+create table student(roll_no int,name varchar(10))
+insert into student values(21,'Abhijeet')
+select * from student
+insert into student values(22,'Abhi')
+select * from student
