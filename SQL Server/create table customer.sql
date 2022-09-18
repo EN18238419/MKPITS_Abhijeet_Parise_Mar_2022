5a47dba2f06bc3e7eba23ad2b8c432ff294fdc90
EN18238419 <abhijeetparise2000@gmail.com>
2022-09-18 22:53:26 +0530

    create table customer

---
+++ SQL Server/create table customer.sql
@@ -0,0 +1,5 @@
+create table customer (id int primary key,name varchar(15))
+insert into customer values(21,'Krishna')
+select * from customer
+insert into customer values(21,'Abhijeet')
+select * from customer
